a6e18f09d1bd3b2695c79835f6ea7c4eebd40f11
Pravesh <pritamyadav70923@gmail.com>
2023-08-10 23:04:16 +0530

    auth context

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,85 +1,91 @@
-import { useState, useRef } from 'react';
+import { useState, useRef ,useContext} from "react";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
+import AuthContext from "../../store/Auth-context";
 
 const AuthForm = () => {
-  const mailInputRef= useRef();
-  const passwordRef=useRef();
+ const authCtx=useContext(AuthContext);
+  const mailInputRef = useRef();
+  const passwordRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
-  const[isLoading,setIsLoading]=useState(false);
-
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
-  const formSubmitHandler=(event)=>{
-
+  const formSubmitHandler = (event) => {
     event.preventDefault();
 
-    const eneterdEmail= mailInputRef.current.value;
-    const eneterdPassword= passwordRef.current.value;
- setIsLoading(true);
-   if(isLogin){
+    const eneterdEmail = mailInputRef.current.value;
+    const eneterdPassword = passwordRef.current.value;
+    setIsLoading(true);
+    let url;
 
-   }
-
-   else{
-    fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAQMsUvpW0VDlrT8udsQOqk9uN4im3NOJA",
-    {
-      method:"POST",
-      body:JSON.stringify({
-        email:eneterdEmail,
-        password:eneterdPassword,
-        returnSecureToken:true
-      }),
-      headers:{
-        "content-type":"application/json"
-      }
-    }).then(res=>{
-      setIsLoading(false);
-      if(res.ok){
-
-      }
-      else{
-        return res.json().then(data=>{
-          console.log(data.error.message)
-        alert(data.error.message)})
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAQMsUvpW0VDlrT8udsQOqk9uN4im3NOJA";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAQMsUvpW0VDlrT8udsQOqk9uN4im3NOJA";
     }
-      
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: eneterdEmail,
+        password: eneterdPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "content-type": "application/json",
+      },
     })
-   }
-
-
-  }
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            // console.log(data.error.message);
+            // alert(data.error.message);
+            throw new Error(data.error.message);
+          });
+        }
+      })
+      .then((data) => {
+        console.log(data.idToken);
+        authCtx.login(data.idToken);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form onSubmit={formSubmitHandler} >
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={formSubmitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input ref={mailInputRef} type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input ref={mailInputRef} type="email" id="email" required />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input
-          ref={passwordRef}
-            type='password'
-            id='password'
-            required
-          />
+          <label htmlFor="password">Your Password</label>
+          <input ref={passwordRef} type="password" id="password" required />
         </div>
         <div className={classes.actions}>
-        {!isLoading &&<button>{isLogin ? "Login" : "Create Account"}</button>}
-        {isLoading && <p>Loading....</p>}
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Loading....</p>}
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,8 +1,11 @@
+import React,{useContext} from 'react';
 import { Link } from 'react-router-dom';
-
 import classes from './MainNavigation.module.css';
+import AuthContext from '../../store/Auth-context';
 
 const MainNavigation = () => {
+  const authCtx=useContext(AuthContext);
+  const isLoggedIn=authCtx.isLoggedIn;
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -10,15 +13,15 @@ const MainNavigation = () => {
       </Link>
       <nav>
         <ul>
-          <li>
+          {!isLoggedIn && <li>
             <Link to='/auth'>Login</Link>
-          </li>
+          </li>}
           <li>
             <Link to='/profile'>Profile</Link>
           </li>
-          <li>
+          {isLoggedIn && <li>
             <button>Logout</button>
-          </li>
+          </li>}
         </ul>
       </nav>
     </header>
